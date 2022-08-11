a476779f548bc889f72434b7db355c593d32fa2a
sarvesh-karki <sarvesh.karki@rumsan.com>
2022-08-11 18:02:55 +0545

    adding mainBanner components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Layouts/Navbar';
+import MainBanner from '../components/DefaultHome/MainBanner';
+import Services from '../components/DefaultHome/Services';
+import About from '../components/DefaultHome/About';
+import OurWorks from '../components/DefaultHome/OurWorks';
 
 class Index extends Component {
     render() {
         return (
             <>
                 <Navbar />
+                <MainBanner/>
+                <Services/>
+                <About/>
+                <OurWorks/>
 
             </>
         );
