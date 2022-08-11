8f1bbe677ca9420e9906940e30275f443f218aed
sarvesh-karki <sarvesh.karki@rumsan.com>
2022-08-12 00:39:21 +0545

    adding loader

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const path = require('path')
 
 module.exports = {
     distDir: 'build',
+    images:{
+        loader:'akamai',
+        path:"",
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
